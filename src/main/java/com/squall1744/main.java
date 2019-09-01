d7fa7eceb06b9fe924fcee597910b9468bc118fb
squall1744 <squall1744@163.com>
2019-09-02 00:22:26 +0800

    Delete white space line
    delete white space line

--- src/main/java/com/squall1744/main.java
+++ src/main/java/com/squall1744/main.java
@@ -9,7 +9,6 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
