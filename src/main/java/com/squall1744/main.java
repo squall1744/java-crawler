64247fefb4d31fe6a6b99c85df83a5c00cffde98
squall1744 <squall1744@163.com>
2019-09-02 00:15:02 +0800

    Finish basic algorithm
    finish basic function about get news from sina.con and print content to console

--- src/main/java/com/squall1744/main.java
+++ src/main/java/com/squall1744/main.java
@@ -1,36 +1,81 @@
 package com.squall1744;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class main {
+    public static void main(String[] args) {
+        saveCrawlerResults();
+    }
 
-    public static void getIndexPage(String url) {
+    private static List<String> linkPool = new ArrayList<>();
+    private static Set<String> handledLink = new HashSet<>();
+
+    public static void saveCrawlerResults() {
+        linkPool.add("https://sina.cn");
+
+        while (!linkPool.isEmpty()) {
+            String url = linkPool.remove(0);
+
+            if (handledLink.contains(url)) {
+                continue;
+            }
+
+            if (isInterestedUrlOrIndexUrl(url)) {
+                getNewsAndSave(url);
+            }
+
+        }
+    }
+
+    public static void getNewsAndSave(String url) {
+
         CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        url = url.startsWith("//") ? ("https:" + url) : url;
+
         HttpGet httpGet = new HttpGet(url);
-        HttpEntity entity = null;
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36");
 
         try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-            System.out.println(response.getStatusLine());
-            entity = response.getEntity();
+            HttpEntity entity = response.getEntity();
 
-            System.out.println(IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8));
+            String html = EntityUtils.toString(entity);
+            Document doc = Jsoup.parse(html);
 
-            EntityUtils.consume(entity);
+            ArrayList<Element> aTags = doc.getElementsByTag("a");
+
+            if (!aTags.isEmpty()) {
+                aTags.forEach(item -> linkPool.add(item.attr("href")));
+            }
+
+            ArrayList<Element> article = doc.getElementsByTag("article");
+
+            if (!article.isEmpty()) {
+                System.out.println(article.get(0).text());
+            }
+
+            handledLink.add(url);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-    public static void main(String[] args) {
-        getIndexPage("https://xiedaimala.com");
+
+    public static boolean isInterestedUrlOrIndexUrl(String url) {
+        return url.contains("news.sina.cn") || url.equals("https://sina.cn");
     }
 }
